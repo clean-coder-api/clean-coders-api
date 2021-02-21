0d8c616209477b0fd94a423e7eb84c9d9d6ba50b
Arman Eskandari <arman_eskandari@live.com>
2021-02-21 14:07:58 +0330

    added cards

--- src/pages/peoples/index.js
+++ src/pages/peoples/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cards from "./cards";
 import Pagination from "./pagination";
@@ -7,6 +7,7 @@ function Peoples(props) {
     const [pageSize] = useState(4);
     const [peoples, setPeoples] = useState([]);
     const [totalNumPages, setTotalNumPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         if (peoples.length <= 0) getPeoples()
@@ -23,10 +24,18 @@ function Peoples(props) {
         setTotalNumPages(Math.ceil(peoples.length / pageSize))
     }
 
+    // Get current peoples
+    const indexOfLastPeople = currentPage * pageSize;
+    const indexOfFirstPeople = indexOfLastPeople - pageSize;
+    const currentPeoples = peoples.slice(indexOfFirstPeople, indexOfLastPeople);
+
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <div className="row justify-content-center">
-            <Cards/>
-            <Pagination totalNumPages={totalNumPages} pageId={props.match.params.id}/>
+            <Cards peoples={currentPeoples} />
+            <Pagination totalNumPages={totalNumPages} pageId={props.match.params.id} paginate={paginate} />
         </div>
     )
 }
